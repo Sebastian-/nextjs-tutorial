edcee555eec9562c5343989e0f8882f59c6fc2e4
sebastian murgu <hmurgu@hotmail.com>
2021-07-15 16:11:50 -0700

    configure swr requests

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,17 @@
-import '../styles/global.css'
+import { SWRConfig } from "swr";
+import "../styles/global.css";
 
 export default function App({ Component, pageProps }) {
-    return <Component {...pageProps} />
-};
+  return (
+    <SWRConfig
+      value={{
+        fetcher: fetch,
+        onError: (err) => {
+          console.log(err);
+        },
+      }}
+    >
+      <Component {...pageProps} />
+    </SWRConfig>
+  );
+}
