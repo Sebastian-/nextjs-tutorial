a275363ac3ed41bf4ce3ab3c20959947f1085a79
sebastian murgu <hmurgu@hotmail.com>
2021-07-16 10:08:36 -0700

    fix useUser setting user data as response object rather than response json

--- pages/_app.js
+++ pages/_app.js
@@ -1,11 +1,12 @@
 import { SWRConfig } from "swr";
+import fetcher from "../lib/fetchJSON";
 import "../styles/global.css";
 
 export default function App({ Component, pageProps }) {
   return (
     <SWRConfig
       value={{
-        fetcher: fetch,
+        fetcher: fetcher,
         onError: (err) => {
           console.log(err);
         },
