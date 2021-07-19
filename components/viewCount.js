403df8851f640368846116883555dc8d87a015e6
sebastian murgu <hmurgu@hotmail.com>
2021-07-19 14:36:25 -0700

    increment a posts' view count on click

--- components/viewCount.js
+++ components/viewCount.js
@@ -3,11 +3,12 @@ import { faEye } from "@fortawesome/free-solid-svg-icons";
 import { useStore } from "../lib/store";
 
 export default function ViewCount({ postId }) {
-  const { count } = useStore((store) => ({
+  const { count, increment } = useStore((store) => ({
     count: store.viewCounts[postId],
+    increment: store.incrementViewCount,
   }));
   return (
-    <span>
+    <span onClick={() => increment(postId)}>
       <FontAwesomeIcon icon={faEye} /> {count}
     </span>
   );
