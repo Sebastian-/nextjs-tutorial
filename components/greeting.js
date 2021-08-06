4e7f88105a87b558c49b720f447eef9a8d28f615
sebastian murgu <hmurgu@hotmail.com>
2021-08-06 13:04:37 -0700

    adjust for use with new useUser hook

--- components/greeting.js
+++ components/greeting.js
@@ -3,7 +3,7 @@ import useUser from '@/lib/useUser'
 export default function Greeting() {
   const { user } = useUser()
 
-  return user?.isLoggedIn ? (
+  return user?.username ? (
     <span>{`Hi ${user.username}!`}</span>
   ) : (
     <span>Hi there!</span>
