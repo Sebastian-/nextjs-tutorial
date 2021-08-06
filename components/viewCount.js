c40c7a28890863f1ef06851cbc64d95d17996c0c
sebastian murgu <hmurgu@hotmail.com>
2021-08-06 13:03:40 -0700

    use absolute import

--- components/viewCount.js
+++ components/viewCount.js
@@ -1,7 +1,7 @@
 import { faEye } from '@fortawesome/free-solid-svg-icons'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 
-import { useStore } from '../lib/store'
+import { useStore } from '@/lib/store'
 
 export default function ViewCount({ postId }) {
   const { count } = useStore((store) => ({
