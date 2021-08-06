3e978032a4e5be54e12d6851b2830bfb691f13c2
sebastian murgu <hmurgu@hotmail.com>
2021-08-06 13:09:48 -0700

    update authLink for new auth flow

--- components/authLink.js
+++ components/authLink.js
@@ -1,30 +1,33 @@
 import Link from 'next/link'
 import { useRouter } from 'next/router'
 
-import fetchJSON from '@/lib/fetchJSON'
-import useUser from '@/lib/useUser'
+import { useStore } from '@/lib/store'
 
 export default function AuthLink() {
-  const { user, mutateUser } = useUser()
+  const [isLoggedIn, setIsLoggedIn] = useStore((state) => [
+    state.isLoggedIn,
+    state.setIsLoggedIn,
+  ])
   const router = useRouter()
   const isAuthPage = router.pathname.includes('/login', '/register')
 
   if (isAuthPage) return null
 
-  return !user?.isLoggedIn ? (
+  return !isLoggedIn ? (
     <Link href='/login'>
       <a>Log In</a>
     </Link>
   ) : (
-    <Link href='/api/logout'>
+    <Link href='/'>
       <a
         onClick={async (e) => {
           e.preventDefault()
-          mutateUser(await fetchJSON('/api/logout', { method: 'POST' }), false)
+          localStorage.removeItem('authToken')
+          setIsLoggedIn(false)
           router.push('/')
         }}
       >
-        Logout
+        Log Out
       </a>
     </Link>
   )
